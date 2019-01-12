b8ba93f3315a2b21023b3a618b06338cbf9e6d10
gotjosh <github@begreen.nu>
2019-01-12 11:36:14 +0000

    fix escaping issue in makepw and basic auth setup

--- scripts/makepw.sh
+++ scripts/makepw.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 USER=${1:-admin}
-PASS=${2:-12345} 
+PASS=${2:-12345}
 
+echo -e "Copy one of the below depending on where you will paste it:\n"
+
+echo '=== for use in docker compose (set traefik.frontend.auth.basic=) ==='
 echo $(htpasswd -nb $USER $PASS) | sed -e s/\\$/\\$\\$/g
+
+echo '=== for use in .env: ==='
+echo "AUTH_STRING=$(htpasswd -nb $USER $PASS)"
