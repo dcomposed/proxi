085293846633f5e8948d8761a517d6a443570e53
sudo <me@1zm.in>
2022-06-18 11:02:13 +0000

    update makepw

--- scripts/makepw.sh
+++ scripts/makepw.sh
@@ -8,12 +8,13 @@ if [[ $2 -eq '-p' ]]; then
   read PASS
 fi
 
-
+AUTHSTR=$(htpasswd -nb $USER $PASS)
 
 echo -e "Copy one of the below depending on where you will paste it:\n"
 
-echo '=== for use in docker compose (set traefik.frontend.auth.basic=) ==='
-echo $(htpasswd -nb $USER $PASS) | sed -e s/\\$/\\$\\$/g
+echo '=== for use in docker compose directly (escaped with double $$) ==='
+DC=$(echo $AUTHSTR | sed -e s/\\$/\\$\\$/g)
+echo "traefik.http.middlewares.auth.basicauth.users: $DC"
 
-echo '=== for use in .env: ==='
-echo "AUTH_STRING=$(htpasswd -nb $USER $PASS)"
+echo -e "\n=== for use in .env ==="
+echo "AUTH_STRING=$AUTHSTR"
